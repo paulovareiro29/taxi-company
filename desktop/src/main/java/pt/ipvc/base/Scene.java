89921375dce52d321c0ace7ba4d6f40adb7605e0
Paulo Vareiro <58229468+paulovareiro29@users.noreply.github.com>
2023-04-14 20:13:19 +0100

    added components to login scene

---
+++ desktop/src/main/java/pt/ipvc/base/Scene.java
@@ -0,0 +1,13 @@
+package pt.ipvc.base;
+
+import javafx.scene.layout.AnchorPane;
+import pt.ipvc.config.Config;
+
+public abstract class Scene extends javafx.scene.Scene {
+
+    public Scene() {
+        super(new AnchorPane(), Config.getWindowWidth(), Config.getWindowHeight());
+        this.getStylesheets().add("/pt/ipvc/styles/global.css");
+
+    }
+}
--- desktop/src/main/java/pt/ipvc/views/LoginScene.java
+++ desktop/src/main/java/pt/ipvc/views/LoginScene.java
@@ -1,21 +1,58 @@
 package pt.ipvc.views;
 
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
-import javafx.scene.layout.AnchorPane;
-import pt.ipvc.config.Config;
+import javafx.geometry.Pos;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import pt.ipvc.base.Scene;
+import pt.ipvc.components.buttons.Button;
+import pt.ipvc.components.buttons.ButtonSize;
+import pt.ipvc.components.inputs.PasswordField;
+import pt.ipvc.components.inputs.TextField;
 
-import java.io.IOException;
+public class LoginScene extends Scene {
 
-public class LoginScene extends Scene{
+    private final TextField usernameField;
+    private final PasswordField passwordField;
 
-    public LoginScene() throws IOException {
-        super(new AnchorPane(), Config.getWindowWidth(), Config.getWindowHeight());
+    public LoginScene() {
+        super();
+        GridPane root = new GridPane();
+        root.setHgap(32);
+        root.setAlignment(Pos.CENTER);
 
-        FXMLLoader loader = new FXMLLoader();
-        loader.setLocation(getClass().getResource("login-view.fxml"));
-        loader.setController(this);
-        setRoot(loader.load());
+        VBox container = new VBox(32);
+        container.setPrefWidth(375);
+        container.setAlignment(Pos.CENTER);
+
+        Label header = new Label("Login");
+        header.setFont(Font.font(null, FontWeight.BOLD,40));
+
+        VBox form = new VBox(8);
+        form.setPrefWidth(container.getPrefWidth());
+
+        usernameField = new TextField();
+        usernameField.setPromptText("Username");
+
+        passwordField = new PasswordField();
+        passwordField.setPromptText("Password");
+
+        Button loginButton = new Button("Login", ButtonSize.LARGE);
+        loginButton.setOnAction(event -> onLogin());
+        loginButton.setMaxWidth(form.getPrefWidth());
+
+
+        // Add components to GUI
+        form.getChildren().addAll(usernameField, passwordField, loginButton);
+        container.getChildren().addAll(header, form);
+        root.getChildren().addAll(container);
+        setRoot(root);
+    }
+
+    private void onLogin() {
+        System.out.println("Tried to login: " + usernameField.getText() + " " + passwordField.getText());
     }
 
 }
