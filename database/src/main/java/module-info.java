0153a7ca55be939d28f1211c6e8d87a2f227649e
Paulo Vareiro <58229468+paulovareiro29@users.noreply.github.com>
2023-04-18 18:57:38 +0100

    added missing important dependencies on database module-info

--- database/src/main/java/module-info.java
+++ database/src/main/java/module-info.java
@@ -1,6 +1,10 @@
 module taxi.company.database.main {
     requires jakarta.persistence;
-    requires org.hibernate.orm.core;
     requires jakarta.xml.bind;
+    requires org.hibernate.orm.core;
+    requires org.hibernate.commons.annotations;
+    requires com.fasterxml.classmate;
+    requires net.bytebuddy;
+    opens pt.ipvc.dal to org.hibernate.orm.core;
     exports pt.ipvc.bll;
 }
