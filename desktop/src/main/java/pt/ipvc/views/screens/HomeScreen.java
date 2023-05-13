f43a9fb1662cfbe440315893825c8c0ac213708b
Paulo Vareiro <58229468+paulovareiro29@users.noreply.github.com>
2023-05-13 14:44:34 +0100

    changed home screen layout

--- desktop/src/main/java/pt/ipvc/views/screens/HomeScreen.java
+++ desktop/src/main/java/pt/ipvc/views/screens/HomeScreen.java
@@ -1,5 +1,10 @@
 package pt.ipvc.views.screens;
 
+import javafx.geometry.HPos;
+import javafx.geometry.Pos;
+import javafx.geometry.VPos;
+import javafx.scene.Group;
+import javafx.scene.layout.*;
 import pt.ipvc.base.Screen;
 import pt.ipvc.components.Heading;
 import pt.ipvc.layout.items.BookingStatistics;
@@ -13,10 +18,30 @@ public class HomeScreen extends Screen {
     public HomeScreen() {
         Heading title = new Heading("Home page");
 
+
+        ColumnConstraints col1 = new ColumnConstraints();
+        ColumnConstraints col2 = new ColumnConstraints();
+        col2.setPercentWidth(50);
+        col1.setPercentWidth(50);
+
+        GridPane grid = new GridPane();
+        grid.getColumnConstraints().addAll(col1, col2);
+        grid.setHgap(8);
+        grid.setVgap(8);
+
         profitStatistics = new ProfitStatistics();
         bookingStatistics = new BookingStatistics();
 
-        this.getChildren().addAll(title, profitStatistics, bookingStatistics);
+        VBox left = new VBox(8);
+        left.getChildren().add(profitStatistics);
+
+        VBox right = new VBox(8);
+        right.getChildren().add(bookingStatistics);
+
+        grid.add(left,0,0);
+        grid.add(right, 1, 0);
+
+        this.getChildren().addAll(title, grid);
     }
 
     @Override
